446a901e5a315c49c6f1763fc6d9c577f9201207
Felipe Parra <fpr29.10@gmail.com>
2020-02-24 11:28:50 -0600

    schema database

---
+++ schema.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "operaciones" ( `fecha` TEXT NOT NULL, `B_Mil` NUMERIC NOT NULL, `B_Quinientos` NUMERIC NOT NULL, `B_Doscientos` NUMERIC NOT NULL, `B_Cien` NUMERIC NOT NULL, `B_Cincuenta` NUMERIC NOT NULL, `B_Veinte` NUMERIC NOT NULL, `Importe` NUMERIC NOT NULL )
